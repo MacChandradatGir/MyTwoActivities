59f5eacdfe1b8e53514f20614a238069ef3bad42
MacChandradatGir <chandradat.gir@mobileappscompany.us>
2019-11-16 03:41:46 -0500

    Update ReadMe - remove images

--- app/src/main/java/com/example/mytwoactivities/SecondActivity.java
+++ app/src/main/java/com/example/mytwoactivities/SecondActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
     public static final String EXTRA_REPLY =
             "com.example.android.twoactivities.extra.REPLY";
     private EditText mReply;
@@ -17,8 +18,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
         mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
+
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
